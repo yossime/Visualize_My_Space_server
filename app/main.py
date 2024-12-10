2050fe120ac6d5c48483fac6fdc4b9d3e94d54e7
yosseymen <yossim@mocart.io>
2024-12-10 15:55:32 +0200

    fix port

--- app/main.py
+++ app/main.py
@@ -23,4 +23,4 @@ app.include_router(download_router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
